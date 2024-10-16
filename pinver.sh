ce2caacbc4495f75272e3094b17ac03b6d65bfe3
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-10-16 13:37:24 +0300

    feat: added script to pin version

---
+++ pinver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i "" -E "s/(FROM ghcr.io\/realm\/swiftlint:).*/\1$1/g" ./Dockerfile
+git add Dockerfile
+git commit -m "chore: swiftlint version $1"
+git tag -a "v$1" -m "swiftlint version $1"
+git push --tags
